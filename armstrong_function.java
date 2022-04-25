3d6d138ddd8f3e8d33708e72c968e840dd3d123c
Kishore <kishorekumar.mahto@vmokshagroup.com>
2022-04-25 15:11:08 +0530

    Armstrong number using static method

---
+++ armstrong_function.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class armstrong_function{
+    public static boolean isarmstrong(int num){
+        int temp=num,pow=0,sum=0;
+        int temp1=num;
+        while(temp>0){
+            pow++;
+            temp=temp/10;
+        }
+
+        while(temp1>0){
+            int position=temp1%10;
+            sum+=Math.pow(position,pow);
+            temp1=temp1/10;
+        }
+
+        if(sum==num){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int num=sc.nextInt();
+        if(isarmstrong(num)){
+            System.out.println(num+" is armstrong number.");
+        }
+        else{
+            System.out.println(num+" is not a armstrong number. ");
+        }
+    }
+}
